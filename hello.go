51c333418517910b30f24e934679d013291f859a
Nicholas <nicholas.zhaoyu@gmail.com>
2016-07-21 22:21:55 -0700

    changes

--- hello.go
+++ hello.go
@@ -2,11 +2,14 @@ package pokemongo
 
 import (
 	"fmt"
+	"github.com/niczy/pokemongolib/handlers"
 	"net/http"
+	"server/handlers"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/_/api", handlers.Api)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
